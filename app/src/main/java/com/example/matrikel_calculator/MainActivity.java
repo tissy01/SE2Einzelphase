a42cb5eeee51e52483abe96a0bf98d9c4fd8e29a
tissy01 <musicraunjak22@gmail.com>
2022-03-07 12:58:24 +0100

    created send methot, editText variable and output stream

--- app/src/main/java/com/example/matrikel_calculator/MainActivity.java
+++ app/src/main/java/com/example/matrikel_calculator/MainActivity.java
@@ -2,11 +2,24 @@ package com.example.matrikel_calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+/**
+ * Raunjak Christian 07.03.2022
+ */
 public class MainActivity extends AppCompatActivity {
+
+    EditText matrikelnr;
 
     @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        matrikelnr = findViewById(R.id.editTextNumber);
+    }
 
+    public void send(View v){
+        Send sender = new Send();
+        sender.execute(matrikelnr.getText().toString());
     }
 }
--- app/src/main/java/com/example/matrikel_calculator/Send.java
+++ app/src/main/java/com/example/matrikel_calculator/Send.java
@@ -4,21 +4,26 @@ import android.os.AsyncTask;
 
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.Socket;
 
 /**
  * Raunjak Christian 07.03.2022
  *
  */
-public class Send extends AsyncTask<Void,Void, Void> {
+public class Send extends AsyncTask<String,Void, Void> {
 
     Socket s;
     DataOutputStream dout;
+    PrintWriter pw;
 
     @Override
-    protected Void doInBackground(Void... voids) {
+    protected Void doInBackground(String... voids) {
+        String matrikelnr = voids[1];
+
         try {
             s = new Socket("se2-isys.aau.at", 53212);
+            pw = new PrintWriter(s.getOutputStream());
         } catch (IOException e) {
             e.printStackTrace();
         }
