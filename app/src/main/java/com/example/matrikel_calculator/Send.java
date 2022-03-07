b650e4baf96d3be20a5ff9bcffe2c5a502cee23c
tissy01 <musicraunjak22@gmail.com>
2022-03-07 12:42:38 +0100

    created client socket and changed main activity to java class

--- app/src/main/java/com/example/matrikel_calculator/Send.java
+++ app/src/main/java/com/example/matrikel_calculator/Send.java
@@ -1,4 +1,27 @@
 package com.example.matrikel_calculator;
 
-public class Send {
+import android.os.AsyncTask;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+/**
+ * Raunjak Christian 07.03.2022
+ *
+ */
+public class Send extends AsyncTask<Void,Void, Void> {
+
+    Socket s;
+    DataOutputStream dout;
+
+    @Override
+    protected Void doInBackground(Void... voids) {
+        try {
+            s = new Socket("se2-isys.aau.at", 53212);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
