35f0a2674847d51e3b9f7dc9f90ea7171dff2bf7
Christian <chraunjak@edu.aau.at>
2023-03-12 15:29:37 +0100

    Creating TextViews, Edit Text and Button with a new button drawable theme

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,10 +1,30 @@
+/*
+    Christian Raunjak
+    01460761
+    SE2 SS 2023
+    Einzelaufgabe 1
+ */
+
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.net.Socket;
 
 public class MainActivity extends AppCompatActivity {
+
+    Context context;
+    Button send;
+    Button calc;
+    Socket socket;
+    EditText input;
+    TextView response;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
