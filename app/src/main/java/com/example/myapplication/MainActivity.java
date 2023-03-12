e057f0a7ee73f8162a273ed4e449597163e58aee
Christian <chraunjak@edu.aau.at>
2023-03-12 17:52:29 +0100

    Added calculate function and button. Some layout and design adjustments.

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -43,9 +43,12 @@ public class MainActivity extends AppCompatActivity {
         send = findViewById(R.id.buttonSend);
         input = findViewById(R.id.numberInput);
         response = findViewById(R.id.serverRspOutput);
+        calc = findViewById(R.id.calculate);
+
+        calc.setOnClickListener(this::alternatingSum);
     }
 
-    public void sendMrtkNr(View view){ // Gets called when clicking on send button, this is defined in the main xml file directly in my button
+    public void sendMrtkNr(View view) { // Gets called when clicking on send button, this is defined in the main xml file directly in my button
         String mrtkNr = input.getText().toString(); // Define String from user input
         ServerStream serverStream = new ServerStream();
         serverStream.execute(mrtkNr); // starts server stream
@@ -62,25 +65,60 @@ public class MainActivity extends AppCompatActivity {
                 socket = new Socket("se2-isys.aau.at", 53212);
 
                 DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-                out.writeBytes(strings[0]+'\n');
+                out.writeBytes(strings[0] + '\n');
                 out.flush();
 
                 BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                 res = in.readLine();
                 socket.close();
                 out.close();
-            }catch (IOException e){
+            } catch (IOException e) {
                 e.printStackTrace();
             }
             return res;
         }
+
         @Override
-        protected void onPostExecute(String res){ //String res is set
+        protected void onPostExecute(String res) { //String res is set
             super.onPostExecute(res);
             response.setText(res);
         }
+
+
     }
 
+    /*
+        2.2 MatrNr: 01460761 mod 7 = 1, Alternierende Quersumme bilden und ausgeben, ob diese gerade oder ungerade ist
+     */
+    public void alternatingSum(View view){
+        try {
+            int mrtkNr = Integer.parseInt(input.getText().toString());
+            int len = input.getText().toString().length();
+            int sum = mrtkNr%10;
+            mrtkNr /= 10;
+            String evenOdd = "";
+
+            for (int i = len-2; i >= 0; i--){
+                if(i%2 == 0){
+                    sum += mrtkNr%10;
+                }
+                else {
+                    sum -= mrtkNr%10;
+                }
+                mrtkNr /= 10;
+            }
+            if (sum %2 == 0){
+                evenOdd = " and the sum is an even number!";
+            }else{
+                evenOdd = " and the sum is an odd number!";
+            }
+            String res = String.valueOf(sum);
+            response.setText("The sum is "+res+evenOdd);
+        }catch (NumberFormatException klaus){
+            response.setText("Invalid Input");
+        }
+
+    }
 
 
 }
